aca6524bb1c7776fbc18acc6c8b7ae04da55c01b
Kolbe Weathington <kswof97@yahoo.com>
2020-03-04 19:47:30 -0600

    ...

--- GR_File_Parser/src/main/kotlin/main.kt
+++ GR_File_Parser/src/main/kotlin/main.kt
@@ -1,59 +1,58 @@
 import java.io.File
 import java.io.FileNotFoundException
+import java.lang.IndexOutOfBoundsException
 
 fun main() {
-    val parser = RecordParser()
     val fileName = RecordParser().javaClass.classLoader.getResource("Test_Input_A.txt")
-    parser.parseFile(File(fileName!!.toURI()))
+    val parser = RecordParser()
+    val records = parser.parseFile(File(fileName!!.toURI()))
+
+
+    val sorted = records!!.sortedBy { it.gender }
+    for(name in sorted){
+        println(name.gender)
+    }
 }
 
 data class Record(
-    val lastName: String = "Doe",
-    val firstName: String = "John",
-    val gender: String = "Male",
-    val favoriteColor: String,
-    val DOB: String = "01/01/1999"
+    var lastName: String = "Doe",
+    var firstName: String = "John",
+    var gender: String = "Male",
+    var favoriteColor: String = "Red",
+    var DOB: String = "01/01/1999"
 )
 
 class RecordParser {
-    private val records = ArrayList<Record>()
-
-    //Parses given file and adds containing records to list
-    fun parseFile(file: File) {
+    fun parseFile(file: File):ArrayList<Record>?{
+        val records = ArrayList<Record>()
         try {
             file.forEachLine {
                 val record = parseLine(it)
-                records.add(record)
+                if (record != null)
+                    records.add(record)
             }
+            return records
         } catch (e: FileNotFoundException) {
             println("File not found!")
         }
-        for (record in records) {
-            print(record.favoriteColor)
-        }
+        return null
     }
 
-    private fun parseLine(line: String): Record {
+    private fun parseLine(line: String): Record? {
         var deliminator = " "
         if (line.contains("|")) deliminator = " | " else if (line.contains(",")) deliminator = ", "
-        val components = line.split(deliminator)
-        return Record(
-            lastName = components[0],
-            firstName = components[1],
-            gender = components[2],
-            favoriteColor = components[3],
-            DOB = components[4]
-        )
-    }
-
-    fun sortRecords(option: Int) {
-        when (option) {
-            0 -> {
-            }
-            1 -> {
-            }
-            2 -> {
-            }
+        try {
+            val components = line.split(deliminator)
+            return Record(
+                lastName = components[0],
+                firstName = components[1],
+                gender = components[2],
+                favoriteColor = components[3],
+                DOB = components[4]
+            )
+        } catch (e: IndexOutOfBoundsException) {
+            println("Index out of bounds! - ParseLine")
         }
+        return null
     }
 }
